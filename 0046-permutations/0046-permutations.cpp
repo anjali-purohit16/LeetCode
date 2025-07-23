e9e8c573858c46a25f6c53e0f50ff3d8286153e1
Anjali Purohit <144976890+anjali-purohit16@users.noreply.github.com>
2025-07-23 15:02:49 -0400

    Time: 0 ms (100%), Space: 10.7 MB (47.93%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,19 @@
 class Solution {
+    vector<vector<int>>ans  ;
+    void solve(vector<int>& nums,int idx){
+        if(idx>=nums.size()){
+            ans.push_back(nums) ;
+            return ;
+        }
+        for(int i=idx ;i<nums.size() ;i++){
+            swap(nums[idx],nums[i]) ;
+            solve(nums,idx+1) ;
+            swap(nums[idx],nums[i])  ;
+        }
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        if (nums.size() == 1) {
-            vector<int> singleList;
-            singleList.push_back(nums[0]);
-            res.push_back(singleList);
-            return res;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            int n = nums[i];
-            vector<int> remainingNums;
-            for (int j = 0; j < nums.size(); j++) {
-                if (j != i) {
-                    remainingNums.push_back(nums[j]);
-                }
-            }
-            
-            vector<vector<int>> perms = permute(remainingNums);
-            for (vector<int> p : perms) {
-                p.insert(p.begin(), n);  // Insert n at the beginning of the permutation
-                res.push_back(p);  // Append the modified permutation to the result
-            }
-        }
-        
-        return res;    
+        solve(nums,0) ;
+        return ans  ;
     }
 };
