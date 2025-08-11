129ffcbaf38e17af6b7a62f6a7333bab48b4b8b9
Anjali Purohit <144976890+anjali-purohit16@users.noreply.github.com>
2025-08-11 23:20:42 +0530

    Time: 9 ms (40.74%), Space: 128.3 MB (79.51%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,56 +1,19 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& A) {
-    vector<int> pos;
-    vector<int> neg;
-  
-  // Segregate the array into positives and negatives.
-  for(int i=0;i<A.size();i++){
-      
-      if(A[i]>0) pos.push_back(A[i]);
-      else neg.push_back(A[i]);
-  }
-  
-  // If positives are lesser than the negatives.
-  if(pos.size() < neg.size()){
-      
-    // First, fill array alternatively till the point 
-    // where positives and negatives ar equal in number.
-    for(int i=0;i<pos.size();i++){
-      
-      A[2*i] = pos[i];
-      A[2*i+1] = neg[i];
+    vector<int> rearrangeArray(vector<int>& nums) {
+    int n=nums.size();
+    vector<int>ans(n,0);
+    int posidx=0 ,negidx=1;
+    for(int i=0;i<n;i++){
+        if(nums[i]<0){
+            ans[negidx]=nums[i];
+            negidx+=2;
+        }
+        else{
+            ans[posidx]=nums[i];
+            posidx+=2;
+        }
     }
-    
-    // Fill the remaining negatives at the end of the array.
-    int index = pos.size()*2;
-    for(int i = pos.size();i<neg.size();i++){
-        
-        A[index] = neg[i];
-        index++;
+    return ans;
     }
-  }
-  
-  // If negatives are lesser than the positives.
-  else{
-      
-      // First, fill array alternatively till the point 
-      // where positives and negatives ar equal in number.
-      for(int i=0;i<neg.size();i++){
-      
-      A[2*i] = pos[i];
-      A[2*i+1] = neg[i];
-  }
-    
-    // Fill the remaining positives at the end of the array.
-    int index = neg.size()*2;
-    for(int i = neg.size();i<pos.size();i++){
-        
-        A[index] = pos[i];
-        index++;
-    }
-  }
-  return A;
-    }
-    
 };
