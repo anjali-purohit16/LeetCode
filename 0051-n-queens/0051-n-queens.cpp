ece0bae5bb582b12f8ce3b694e39f8a774f45b12
Anjali Purohit <144976890+anjali-purohit16@users.noreply.github.com>
2025-11-01 23:19:28 +0530

    Time: 3 ms (65.26%), Space: 12.5 MB (15.93%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,65 +1,51 @@
 class Solution {
 public:
-        vector<vector<string>>ans;
-        vector<vector<string>> solveNQueens(int n){
-            vector<vector<int>> grid(n,vector<int>(n,0));
-            solve(grid,0);
-            return ans;
+    vector<bool> leftDiagonal;
+    vector<bool> rightDiagonal;
+    vector<bool> column;
+    vector<vector<string>> out;
+    vector<string> current;
+
+    void helper(int n, int size){
+        if(n == size){
+            out.push_back(current);
+            return;
         }
-    void addAns(vector<vector<int>>&grid){
-        vector<string>ls;
-        for(int i=0;i<grid.size();i++){
-            string curr ="";
-            for(int j=0;j<grid.size();j++){
-                if(grid[i][j]==0){
-                    curr+=".";
-                }
-                else{
-                    curr+="Q";
-                }
+        string row(size, '.');
+
+        for(int i = 0;i < size;i++){
+            if(leftDiagonal[n + i] || rightDiagonal[size + (n - i)] || column[i]){
+                continue;
             }
-            ls.push_back(curr);
-        }
-        ans.push_back(ls);
-    }
 
-    void solve(vector<vector<int>>& grid,int row)
-    {
-     //base case
-        if(grid.size()==row){
-        addAns(grid);
-        return;
-       }
-     for(int j=0;j<grid.size();j++)
-     {
-         if(ispossible(row,j,grid)){
-         grid[row][j]=1;
-         solve(grid,row+1);
-         //backtrack
-         grid[row][j]=0;
-        }
-      }
-    }
+            leftDiagonal[n + i] = true;
+            rightDiagonal[size + (n - i)] = true;
+            column[i] = true;
 
-    bool ispossible(int row,int col ,vector<vector<int>>& grid){
-        int x=row;
-        int y=col;
-        while(x>=0){
-            if(grid[x--][y]==1)return false;
-        }
-        x=row;
-        y=col;
-        while(x >= 0 && y >= 0){
-            if(grid[x--][y--]==1)return  false;
+            row[i] = 'Q';
+            current.push_back(row);
+
+            helper(n + 1, size);
+
+            row[i] = '.';
+            current.pop_back();
+
+            leftDiagonal[n + i] = false;
+            rightDiagonal[size + (n - i)] = false;
+            column[i] = false;
+
+
         }
 
-       x=row;
-        y=col;
-        while(x >= 0 && y < grid.size()){
-            if(grid[x--][y++]==1)return  false;
-    }
-    return true;
     }
 
-    
+    vector<vector<string>> solveNQueens(int n) {
+        leftDiagonal = vector<bool>(n + 1);
+        rightDiagonal = vector<bool>(2 * n + 1);
+        column = vector<bool>(n + 1);
+
+        helper(0, n);
+
+        return out;
+    }
 };
