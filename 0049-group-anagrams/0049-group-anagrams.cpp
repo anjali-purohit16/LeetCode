dfad02f40f2fba020acf83091403188dc050dc45
Anjali Purohit <144976890+anjali-purohit16@users.noreply.github.com>
2026-01-16 00:05:20 +0530

    Time: 18 ms (51.05%), Space: 24.9 MB (76.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-     unordered_map<string, vector<string>> mp;
-    for (auto &s : strs) {
-        string key = s;
-        sort(key.begin(), key.end());
-        mp[key].push_back(s);
+        unordered_map<string ,vector<string>>mpp;
+        vector<vector<string>>result;
+        for(int i=0;i<strs.size();i++){
+            string temp=strs[i];
+            sort(begin(temp),end(temp));
+            mpp[temp].push_back(strs[i]);
+            }
+        for(auto &it : mpp){
+            result.push_back(it.second);
+        }
+        return result;
     }
-    vector<vector<string>> res;
-    res.reserve(mp.size());
-    for (auto &p : mp) res.push_back(move(p.second));
-    return res;
-}
-
 };
